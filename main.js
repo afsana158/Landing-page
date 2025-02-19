fd20bddff7116db92174a9487c00a12a175b378a
afsana158 <afsanasoha38@gmail.com>
2025-02-19 18:33:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const navLinks = document.getElementById("links");
+const menuBtn = document.getElementById("menu_button"); 
+const menuBtnIcon = menuBtn.querySelector("i");
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-3-line");
+    });
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+    
+    });
+
+const scrollOption = {
+    distance: "40px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+//header container
+ScrollReveal().reveal(".header_content h1", { ...scrollOption, delay : 100});
+
+//about container
+ScrollReveal().reveal(".about_content .sec_header", { ...scrollOption,});
+ScrollReveal().reveal(".about_content .description", { ...scrollOption, delay : 500});
+
+//skills container
+ScrollReveal().reveal(".skills_content .sec_header", { ...scrollOption,});
+ScrollReveal().reveal(".skills_content .skills", { ...scrollOption, delay : 500});
